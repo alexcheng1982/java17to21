c42849daadf845810f19aca8b28d454deb227841
Fu Cheng <alexcheng1982@gmail.com>
2023-10-29 23:55:29 +0800

    scoped value

--- src/main/java/io/vividcode/java17to21/scopedvalue/SimpleScopedValue.java
+++ src/main/java/io/vividcode/java17to21/scopedvalue/SimpleScopedValue.java
@@ -3,6 +3,7 @@ package io.vividcode.java17to21.scopedvalue;
 public class SimpleScopedValue {
 
   private final static ScopedValue<String> VALUE = ScopedValue.newInstance();
+  private final static ScopedValue<String> UNBOUND = ScopedValue.newInstance();
 
   public void print() {
     ScopedValue.runWhere(VALUE, "hello", () -> {
@@ -10,7 +11,33 @@ public class SimpleScopedValue {
     });
   }
 
+  public void call() throws Exception {
+    ScopedValue.callWhere(VALUE, "hello",
+        () -> VALUE.get().toUpperCase());
+  }
+
+  public void get() {
+    ScopedValue.getWhere(VALUE, "hello",
+        () -> VALUE.get() + " world");
+  }
+
+  public void getOrElse() throws Exception {
+    ScopedValue.callWhere(VALUE, "hello", () ->
+        VALUE.get() + UNBOUND.orElse("default"));
+  }
+
+  public void rebound() {
+    ScopedValue.runWhere(VALUE, "hello", () -> {
+      var result = ScopedValue.getWhere(VALUE, "world",
+          () -> VALUE.get().toUpperCase());
+      System.out.println(result);
+    });
+  }
+
+
   public static void main(String[] args) {
-    new SimpleScopedValue().print();
+    var sample = new SimpleScopedValue();
+    sample.print();
+    sample.rebound();
   }
 }
